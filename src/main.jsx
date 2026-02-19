c072bb4929abbd9d90db8dc9b1967ee79d66351b
scott-taha <scott.taha21@gmail.com>
2026-02-19 02:08:43 +0000

    Manual build deploy

--- src/main.jsx
+++
@@ -1,8 +0,0 @@
-import { createRoot } from "react-dom/client";
-import App from "./app/App.tsx";
-import "./styles/index.css";
-
-const rootElement = document.getElementById("root");
-if (rootElement) {
-    createRoot(rootElement).render(<App />);
-}
